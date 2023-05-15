1af5b4e713470c42259bf475f29924397debe9ec
ravioli <lucj6778@gmail.com>
2023-05-15 17:00:17 +0200

    Ajout d'un méthode create et delete

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,6 +7,7 @@ use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,16 +21,31 @@ class ProduitController extends AbstractController
         ]);
     }
 
-    #[Route('/create', name: 'produit_create')]
-    public function create(EntityManagerInterface $manager){
+    #[Route('/admin/create', name: 'produit_create')]
+    public function create(Request $request, EntityManagerInterface $manager): Response{
         $produit = new Produit();
         $form = $this->createForm(ProduitType::class, $produit);
 
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($produit);
+            $manager->flush();
+            return $this->redirectToRoute('app_produit');
+        }
 
         return $this->renderForm('produit/create.html.twig', [
             'form'=>$form
         ]);
+    }
+
+    #[Route('/admin/delete{id}', name: 'produit_delete')]
+    public function delete(Produit $produit, EntityManagerInterface $manager){
+        if ($produit){
+            $manager->remove($produit);
+            $manager->flush();
+        }
+        return $this->redirectToRoute('app_produit');
     }
 
     #[Route('/show/{id}', name: 'produit_show')]
