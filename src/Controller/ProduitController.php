de79d8f4bf2957772015945033e5d647b79bae00
ravioli <lucj6778@gmail.com>
2023-05-15 11:39:14 +0200

    First test commit

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/', name: 'app_produit')]
+    public function index(ProduitRepository $repository): Response
+    {
+        return $this->render('produit/index.html.twig', [
+            'produits' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: 'produit_create')]
+    public function create(EntityManagerInterface $manager){
+        $produit = new Produit();
+        $form = $this->createForm(ProduitType::class, $produit);
+
+
+
+        return $this->renderForm('produit/create.html.twig', [
+            'form'=>$form
+        ]);
+    }
+}
